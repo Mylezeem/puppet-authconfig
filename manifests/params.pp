5089d6898f5c713baedd058ea7ed9b9cd5d820c5
Darrin Chambers <DarrinC@users.noreply.github.com>
2014-06-06 08:52:01 -0700

    Update params.pp
    add nis_packages & services

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,12 @@ class authconfig::params () {
 
   $packages = ['authconfig']
   $cache_packages = ['nscd']
-  $ldap_packages = ['openldap-clients', 'nss-pam-ldapd', 'pam_ldap']
-  $krb5_packages = ['pam_krb5', 'krb5-workstation']
-  $services = []
+  $ldap_packages  = ['openldap-clients', 'nss-pam-ldapd', 'pam_ldap']
+  $krb5_packages  = ['pam_krb5', 'krb5-workstation']
+  $nis_packages   = ['ypbind']
+  $nis_services   = ['ypbind']
+  $services       = []
   $cache_services = ['nscd']
-  $ldap_services = ['nslcd']
+  $ldap_services  = ['nslcd']
 
 }
