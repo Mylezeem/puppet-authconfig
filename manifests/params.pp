eb95119b8524b6a1703b6e529d8b504d72f41c03
Gilles Gagniard <gilles@gagniard.org>
2014-09-05 20:36:17 +0200

    list of packages to install has changed on rhel7

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,10 @@ class authconfig::params () {
 
   $packages = ['authconfig']
   $cache_packages = ['nscd']
-  $ldap_packages  = ['openldap-clients', 'nss-pam-ldapd', 'pam_ldap']
+  $ldap_packages  = $::operatingsystemmajrelease ? {
+    7 => ['openldap-clients', 'nss-pam-ldapd'],
+    default => ['openldap-clients', 'nss-pam-ldapd', 'pam_ldap']
+  }
   $krb5_packages  = ['pam_krb5', 'krb5-workstation']
   $nis_packages   = ['ypbind']
   $nis_services   = ['ypbind']
